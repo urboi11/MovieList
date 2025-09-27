65f08b28500e8e5cc8f2374d443dfabdd9c2a9d9
Jason Welsh <132491235+urboi11@users.noreply.github.com>
2025-09-27 06:22:14 -0400

    Adding More Files.

--- MovieList/Models/MovieContext.cs
+++ MovieList/Models/MovieContext.cs
@@ -6,7 +6,34 @@ namespace MovieList.Models
     public class MovieContext : DbContext
     {
         public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
-        
+
         public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    MovieId = 1,
+                    Name = "Casablanca",
+                    Year = 1942,
+                    Rating = 5
+                },
+                new Movie
+                {
+                    MovieId = 2,
+                    Name = "Wonder Woman",
+                    Year = 2017,
+                    Rating = 3
+                },
+                new Movie
+                {
+                    MovieId = 3,
+                    Name = "Moonstruck",
+                    Year = 1988,
+                    Rating = 4
+                }
+            );
+        }
     }
 }
--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -1,8 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddDbContext<MovieContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
